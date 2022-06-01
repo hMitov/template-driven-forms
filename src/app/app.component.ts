4e5900ea4544967edf09af1e6d00619bbd3323ba
hmitov <hmitov@hedgeserv.com>
2022-06-01 13:23:28 +0300

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template-driven-forms';
+  @ViewChild('f') signUpForm: NgForm;
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+  onSubmit() {
+
+  }
 }
